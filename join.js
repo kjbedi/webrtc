cf350930b3f9f240d255215860c02baf8fcc31ee
fun_with_web_rtc <jasraj3453@gmail.com>
2017-12-30 22:02:38 -0500

    implemented
    - displaying number of users if new connection is made.
    - if a person leaves then the code works sometimes and sometimes it does not work(needs to be updated)

--- join.js
+++ join.js
@@ -1,6 +1,3 @@
-
-
-
 var connection = new RTCMultiConnection();
 
 // this line is VERY_important
@@ -12,29 +9,72 @@ connection.session = {
 	video:true,	
 };
 
-
+// connection.language = 'fr';
+// connection.autoTranslateText = true;
 
 function get_users(){
 	connection.getAllParticipants().forEach(function(participantId) {
         var user = connection.peers[participantId];
         var hisUID = user.userid;
-       // alert(hisUID + ' connected with you.');
     });
 
     var numberOfUsers = connection.getAllParticipants().length;
-    //alert(numberOfUsers + ' users connected with you.');
-	connection.send(numberOfUsers);
-	document.getElementById("abc").innerHTML = "users:"+numberOfUsers;
+	var arrayOfUserIds = connection.getAllParticipants();
+	var return_arr = {num_users:numberOfUsers,all_users:arrayOfUserIds};
+	return return_arr;
+}
+
+function send_msg_for_new_connection_to_others(){
+	var data = {type:"update_num_of_users"};
+	connection.send(data);
+}
+
+
+connection.onstream = function(event) {
+    var video = event.mediaElement;
+    document.body.appendChild(video);
+	send_msg_for_new_connection_to_others();
 	
+	var user_data = get_users();
+		
+	var numberOfUsers = user_data.num_users;
+	var arrayOfUserIds = user_data.all_users;
+	update_users(arrayOfUserIds);
+};
+
+function update_users(users){
+	document.getElementById("ids").innerHTML = "";
+	users.forEach(function(id) {	
+		var userid = document.createElement("p");    
+		userid.innerHTML = id;
+		document.getElementById("ids").appendChild(userid);
+	});
 }
 
 
 connection.onmessage =  function(event) {
     var buffer = event.data;
-	
-	document.getElementById("abc").innerHTML = "users:"+buffer;
+	if(buffer.type == "update_num_of_users"){
+		var user_data = get_users();
+		
+		var numberOfUsers = user_data.num_users;
+		var arrayOfUserIds = user_data.all_users;
+		update_users(arrayOfUserIds);
+	}
 };
 
-connection.join('your-room-id');
+connection.onclose = function(e) {
+	var user_data = get_users();
+		
+	var numberOfUsers = user_data.num_users;
+	var arrayOfUserIds = user_data.all_users;
+	setTimeout(update_users(arrayOfUserIds), 3000);
+};
+
+
+
+
+connection.join('kjbedi');
+
 
 
--- main.js
+++ main.js
@@ -12,17 +12,50 @@ connection.session = {
 	video:true,
 	
 };
-function a(){
-	connection.send("hello");
+
+
+function get_users(){
+	connection.getAllParticipants().forEach(function(participantId) {
+		var user = connection.peers[participantId];
+		var hisUID = user.userid;
+    });
+
+    var numberOfUsers = connection.getAllParticipants().length;
+	var arrayOfUserIds = connection.getAllParticipants();
+	var return_arr = {num_users:numberOfUsers,all_users:arrayOfUserIds};
+	
+	return return_arr;
 }
 
+function update_users(users){
+	document.getElementById("ids").innerHTML = "";
+	users.forEach(function(id) {	
+		var userid = document.createElement("p");    
+		userid.innerHTML = id;
+		document.getElementById("ids").appendChild(userid);
+	});
+}
+
+connection.onclose = function(e) {
+	var user_data = get_users();	
+	var numberOfUsers = user_data.num_users;
+	var arrayOfUserIds = user_data.all_users;
+	update_users(arrayOfUserIds);
+};
 
 
 
 connection.onmessage =  function(event) {
     var buffer = event.data;
-	document.getElementById("abc").innerHTML = "users:"+buffer;
+	if(buffer.type == "update_num_of_users"){
+		var user_data = get_users();
+		
+		var numberOfUsers = user_data.num_users;
+		var arrayOfUserIds = user_data.all_users;
+		update_users(arrayOfUserIds);
+	}
 };
-connection.open('your-room-id');
+
+connection.open('kjbedi');
 
 
